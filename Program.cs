74abd5247a4de5de6227a711caedef28837aac18
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-07 17:22:04 -0300

    enviei  Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+// String, Date, Int, bool, double, Guid;
+namespace Tp3{
+    public class Program{
+
+        public static void Main(String[] args){
+
+         Incluir mostrar =  new Incluir();
+
+        mostrar.exibirInput();
+ }
+    }
+}
