863b20c0eee023c22d5defa8f51b7e7fba2b5487
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-08 07:57:18 -0300

    Coloquei o código mais "limpo" e deletei um comentário desnecessário

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
-// String, Date, Int, bool, double, Guid;
-namespace Tp3{
-    public class Program{
 
+namespace Tp3{
+    
+    public class Program{
+        
         public static void Main(String[] args){
 
          Incluir mostrar =  new Incluir();
 
         mostrar.exibirInput();
- }
+    
+        }
     }
 }
